c66f21e5422c6b35c179f706105e5ae546cd8a7e
Bruce Szalwinski <szalwinski@csdisco.com>
2022-12-02 16:53:30 -0600

    part2

--- 2022/day2/day2.sh
+++ 2022/day2/day2.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 part1() {
-  join -j 2  <( cat input| sed 's/ //g' | sort | uniq -c) <(cat converter) | awk '{print $2*$3}' | paste -sd+ - | bc
+  join -j 2  <( cat input| sed 's/ //g' | sort | uniq -c) <(cat converter1 | grep -v "#") | awk '{print $2*$3}' | paste -sd+ - | bc
+}
+
+part2() {
+   join -j 2  <( cat input| sed 's/ //g' | sort | uniq -c) <(cat converter2 | grep -v "#"| sort -k 2) | awk '{print $2, $3}' | sort -k 2 | join -j 2 <(cat converter1 | grep -v "#") - | awk '{print $2*$3}' | paste -sd+ - | bc
 }
 
 part1
+part2
