a5da2b2719c8dda5308b761c37c244ae0f55f6ff
Bruce Szalwinski <szalwinski@csdisco.com>
2022-12-03 11:24:26 -0600

    chunked

--- 2022/src/day3/day3.py
+++ 2022/src/day3/day3.py
@@ -1,17 +1,17 @@
+from more_itertools import chunked
 
-priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+priorities = ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def rucksacks():
     with open("input") as file:
-        line_list = file.readlines()
-        return [item.rstrip() for item in line_list]
+        sack = file.readlines()
+        return [item.rstrip() for item in sack]
 
 
 def part1():
     sum_of_priorities = 0
-    sacks = rucksacks()
-    for s in sacks:
+    for s in rucksacks():
         c1 = slice(0, len(s) // 2)
         c2 = slice(len(s) // 2, len(s))
 
@@ -19,30 +19,13 @@ def part1():
             set(s[c1]).intersection(s[c2])
         )
 
-        sum_of_priorities += list(priorities).index(common) + 1
+        sum_of_priorities += list(priorities).index(common)
     return sum_of_priorities
 
 
 def part2():
-    sum_of_priorities = 0
-
-    sacks = rucksacks()
-    groups = []
-    i = 1
-    for s in sacks:
-        if len(groups) == i:
-            groups[i-1].append(s)
-        else:
-            groups.append([s])
-
-        if sacks.index(s) % 3 == 2:
-            i += 1
-
-    for g in groups:
-        common = ''.join(set(g[0]).intersection(g[1]).intersection(g[2]))
-        sum_of_priorities += list(priorities).index(common) + 1
-
-    return sum_of_priorities
+    groups = chunked(rucksacks(), 3)
+    return sum([list(priorities).index(''.join(set(g[0]).intersection(g[1]).intersection(g[2]))) for g in groups])
 
 
 if __name__ == '__main__':
